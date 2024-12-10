9087c2f66c4d05f1c74452831ff7b79cfe218e42
asereje12 <135567771+asereje12@users.noreply.github.com>
2024-12-10 14:14:43 -0500

    Produccion.py

--- Produccion.py
+++ Produccion.py
@@ -10,15 +10,15 @@ st.title("Modelo de Producción: Stacking Model")
 st.write("Valores requeridos")
 
 #Datos
-AñosEstudio=st.number_input("Año de Estudio",min_value=0.0, step=0.1)
-HorasSemanales=st.number_input("Horas Semanales",min_value=0.0, step=0.1)
-NumTrabajadores=st.number_input("Numero de Trabajadores",min_value=0.0, step=0.1)
+AñosEstudio=st.number_input("Año de Estudio",min_value=0.0, step=1.0)
+HorasSemanales=st.number_input("Horas Semanales",min_value=0.0, step=1.0)
+NumTrabajadores=st.number_input("Numero de Trabajadores",min_value=0.0, step=1.0)
 
 #Botón para predecir
 if st.button("Predecir"):
   try:
     input=pd.DataFrame([[AñosEstudio,HorasSemanales,NumTrabajadores]],columns=["AñosEstudio: Años de Estudio", "HorasSemanales: Horas Semanales", "NumTrabajadores:N de trabajadores"])
-    predict= Stacking.predict(input_data)
+    predict= Stacking.predict(input)
     st.success(f"Prediccion: {predict[0]}")
   except Exception as e:
     st.error(f"Error: {e}")
