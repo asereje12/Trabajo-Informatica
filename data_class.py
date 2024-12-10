043d51debd9a93ce3dbb8c39aca4a11245221807
asereje12 <135567771+asereje12@users.noreply.github.com>
2024-12-10 01:06:01 -0500

    data_class.py

--- data_class.py
+++ data_class.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import LabelEncoder
-
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
